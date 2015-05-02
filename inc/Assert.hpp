1f0d14f78fd58dc42dc2bf1d1cc40573f1774fb5
Rostakagmfun <rostawesomegd@gmail.com>
2015-05-02 20:59:57 +0300

    Added V_ASSERT macro

---
+++ inc/Assert.hpp
@@ -0,0 +1,11 @@
+#ifndef ASSERT_H
+#define ASSERT_H
+#include <OS.hpp>
+
+#define v_ASSERT(expr) do { if(!(expr)) { \
+    OS::get()->Log("v_ASSERT("#expr") failed in %s, on %s, in %s", \
+            __FILE__,__LINE__,__PRETTY_FUNCTION__); \
+    exit(1); \
+}} while(0) 
+
+#endif //ASSERT_H
